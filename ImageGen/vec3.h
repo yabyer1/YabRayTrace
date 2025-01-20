01fbc40d1471f4de385cdd83268f50e3c8dc1386
Abhay Yer <abhay.iyer7@gmail.com>
2025-01-19 20:04:57 -0600

    fix vector header file

--- ImageGen/vec3.h
+++ ImageGen/vec3.h
@@ -63,10 +63,10 @@ public:
     inline double dot(const vec3& u, const vec3& v){
         return u.e[0] *v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
     }
-    inline double cross(const vec3& u, const vec3& v){
+    inline vec3 cross(const vec3& u, const vec3& v){
         return vec3(u.e[1] *v.e[2] - u.e[2] * v.e[1],
                  u.e[2] * v.e[0] - u.e[0] * v.e[2],
-                u.e[0] * v.e[1] - u.e[1] * v.e[1]);
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
     }
     inline vec3 unit_vector(const vec3& v){
         return v / v.length();
@@ -75,5 +75,5 @@ public:
     
     
     
-}
+};
 #endif
