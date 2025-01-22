7da5a8dfdc7489e191156b3c62da596c7c2b1c12
Abhay Yer <abhay.iyer7@gmail.com>
2025-01-22 17:02:13 -0600

    sphere colored via normal vector

--- ImageGen/main.cc
+++ ImageGen/main.cc
@@ -2,18 +2,24 @@
 #include "vec3.h"
 #include "color.h"
 #include "ray.h"
-bool hit_sphere(const point3& center, double radius, const ray& r){
+double hit_sphere(const point3& center, double radius, const ray& r){
     vec3 oc = center - r.origin();
     auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius * radius;
     auto discriminant = b * b - 4*a*c;
-    return (discriminant >= 0);
+    if(discriminant < 0)
+        return -1.0;
+    return (-b - (std:: sqrt(discriminant))) / (2.0 * a);
 }
 color ray_color(const ray& r){
     //blended value = (1 - a) * startValue + a * endvalue
-   if(hit_sphere(point3(0,0, -1), 0.5, r))
-       return color(1, 0 , 0);
+    auto t = hit_sphere(point3(0,0, -1), 0.5, r);
+    if(t > 0.0){
+        vec3 N = unit_vector(r.at(t) - vec3(0,0, -1));
+        return 0.5* color(N.x() + 1, N.y() + 1, N.z() + 1);
+
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
