9b5e03ae47723f65754273cabc67eab9a5caeb58
Abhay Yer <abhay.iyer7@gmail.com>
2025-01-22 16:42:19 -0600

    sphere works

--- ImageGen/main.cc
+++ ImageGen/main.cc
@@ -2,17 +2,26 @@
 #include "vec3.h"
 #include "color.h"
 #include "ray.h"
+bool hit_sphere(const point3& center, double radius, const ray& r){
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4*a*c;
+    return (discriminant >= 0);
+}
 color ray_color(const ray& r){
     //blended value = (1 - a) * startValue + a * endvalue
+   if(hit_sphere(point3(0,0, -1), 0.5, r))
+       return color(1, 0 , 0);
     vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5 * (unit_direction.y() + 1.0); // a must be in the range [0, 1], so we need to normalize direction vector giving it [-1, 1] then add 1 and multiply by .5
-    //gradient from color1 to color2
-    return (1.0 - a ) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 int main(){
     auto aspect_ratio = 16.0 / 9.0;
     
-    int image_width = 256;
+    int image_width = 400;
    
     //image height from width and ratio and must be 1 at the very least
     int image_height = (int) (image_width / aspect_ratio);
@@ -42,7 +51,7 @@ int main(){
             color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
     // write out the color
-                                      write_color(std::cout, pixel_color);
+                            
         }
     }
 }
